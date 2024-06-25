86f5c9179470069cee933003793a47f86ad07e77
Patrik Simic <patrik.simic@gmail.com>
2024-06-25 16:24:54 +0200

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import round1 from "$lib/data/round1.json";
   import round2 from "$lib/data/round2.json";
+  import round3 from "$lib/data/round3.json";
 </script>
   
 <div class="max-w-7xl mx-auto py-8 sm:py-16 sm:px-6 lg:px-8">
@@ -10,7 +11,8 @@
     <h1 class="lg:p-10 text-3xl text-gray-600">Round 1</h1>
   </div>
   
-  <div class="lg:border-dashed lg:rounded-md lg:border-gray-400 lg:border-2" >
+  <!--div class="lg:border-dashed lg:rounded-md lg:border-gray-400 lg:border-2" -->
+  <div>
     <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
       {#each round1 as game}
         <li>
@@ -42,9 +44,41 @@
     </div>
   </div>
 
-  <div class="lg:border-dashed lg:rounded-md lg:border-gray-400 lg:border-2" >
+  <div>
     <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
       {#each round2 as game}
+        <li>
+          <a href={`/${game.id}`}>
+            <div class="flex flex-col w-48 p-4 border-solid border-2 rounded-lg">
+            <div class="flex">
+              <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag1}`} alt={game.team1} />
+              <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag2}`} alt={game.team2} />
+            </div>
+            <div class="space-y-2">
+              <div class="text-xs font-medium lg:text-sm text-center">
+                <h3>
+                  <div class="text-black">{game.team1} - {game.team2}</div>
+                  <div class="text-black">{game.result}</div>
+                </h3>
+              </div>
+            </div>
+            </div>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
+  <!-- round 3 -->
+  <div class="sm:flex sm:items-center">
+    <div class="sm:flex-auto">
+      <h1 class="lg:p-10 text-3xl text-gray-600 text-center">Round 3</h1>
+    </div>
+  </div>
+
+  <div>
+    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
+      {#each round3 as game}
         <li>
           <a href={`/${game.id}`}>
             <div class="flex flex-col w-48 p-4 border-solid border-2 rounded-lg">
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -7,22 +7,42 @@
   }
 
   import round1 from "$lib/data/round1.json";
+  import round2 from "$lib/data/round2.json";
+  import round3 from "$lib/data/round3.json";
 
-  let game = round1[id - 1];
+  let idx = parseInt(id, 10)
+  
+  let game = round1[idx - 1];
+
+  if (idx > 12 && idx <= 24) {
+    game = round2[idx - 13];
+  }
+
+  if (idx >= 25) {
+    game = round3[idx - 25];
+  }
+  
 </script>
 
 {#if game}
 <div class="my-10 mx-auto lg:w-1/2 bg-white shadow overflow-hidden sm:rounded-lg">
   <div class="px-4 py-5 sm:px-6">
-    <div class="text-2xl leading-6 font-medium">{game.team1} - {game.team2}</div>
-    <div class="py-2 text-2xl leading-6 font-medium">{game.result}</div>
-</div>
+    <div class="text-2xl leading-6 font-medium">{game.team1} {game.result1} - {game.team2} {game.result2}</div>
+  </div>
   <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
     {#each game.links as link}
     <dl class="sm:divide-y sm:divide-gray-200">
       <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">{link.currentresult}</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{link.snippet}</dd>
+        <dt class="text-sm font-medium text-gray-600">
+          <a rel="external" href="{link.link}" class="text-teal-800 space-y-4">
+            {game.team1} {link.currentresult1} - {game.team2} {link.currentresult2}
+          </a>
+        </dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">
+          <video controls>
+            <source src="{link.video}" type="video/mp4">
+          </video>
+        </dd>
       </div>
     </dl>
     {/each}
