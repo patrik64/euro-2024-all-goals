436c2dc512fc9b650cdd4dca4ce2ace83c5a4b4c
Patrik Simic <patrik.simic@gmail.com>
2024-06-25 13:43:08 +0200

    links added

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  import Youtube from "svelte-youtube-embed";
   export let data;
 
   let id = '';
@@ -7,69 +6,26 @@
     id += data[x];
   }
 
-  import movies from "$lib/data/movies.json";
+  import round1 from "$lib/data/round1.json";
 
-  let movie = movies[id - 1];
+  let game = round1[id - 1];
 </script>
 
-{#if movie}
+{#if game}
 <div class="my-10 mx-auto lg:w-1/2 bg-white shadow overflow-hidden sm:rounded-lg">
   <div class="px-4 py-5 sm:px-6">
-    <div class="text-2xl leading-6 font-medium">{movie.name}</div>
-    {#if movie.image && movie.image.length > 0}
-      <img class="my-4 object-contain" src={`/images/movies/${movie.image}`} alt="" />
-      <div class="text-xss text-gray-500">&copy; imdb</div>
-    {/if}
-  </div>
+    <div class="text-2xl leading-6 font-medium">{game.team1} - {game.team2}</div>
+    <div class="py-2 text-2xl leading-6 font-medium">{game.result}</div>
+</div>
   <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+    {#each game.links as link}
     <dl class="sm:divide-y sm:divide-gray-200">
       <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Name</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.name}</dd>
+        <dt class="text-sm font-medium text-gray-600">{link.currentresult}</dt>
+        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{link.snippet}</dd>
       </div>
-      {#if movie.name !== movie.engname}
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">ENG Name</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.engname}</dd>
-      </div>
-      {/if}
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">IMDB Score</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.score}</dd>
-      </div>
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Year</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.year}</dd>
-      </div>
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Movie Length</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.length}</dd>
-      </div>
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">IMDB</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2"><a href={movie.links[1].lvalue} class="hover:text-black">{movie.links[1].lvalue}</a></dd>
-      </div>
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Viennale</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2"><a href={movie.links[0].lvalue} class="hover:text-black">{movie.links[0].lvalue}</a></dd>
-      </div>
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Show 1</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.shows[0].date} - {movie.shows[0].time}h - {movie.shows[0].weekday} - {movie.shows[0].kino}</dd>
-      </div>
-      {#if movie.shows[1].kino.length > 0}
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Show 2</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2">{movie.shows[1].date} - {movie.shows[1].time}h - {movie.shows[1].weekday} - {movie.shows[1].kino}</dd>
-      </div>
-      {/if}
-      {#if movie.trailer}
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-600">Trailer</dt>
-        <dd class="mt-1 text-sm text-gray-600 sm:mt-0 sm:col-span-2"><Youtube id="{movie.trailer}" /></dd>
-      </div>
-      {/if}
     </dl>
+    {/each}
   </div>
 </div>
 {/if}
