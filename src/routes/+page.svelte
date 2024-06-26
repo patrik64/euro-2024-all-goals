8a206e260d8f2e05e970f096058e45a469027e50
Patrik Simic <patrik.simic@gmail.com>
2024-06-26 20:52:25 +0200

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,12 @@
   <div class="flex justify-center items-center">
     <h1 class="lg:p-10 text-3xl text-gray-600">Round 1</h1>
   </div>
-  
-  <!--div class="lg:border-dashed lg:rounded-md lg:border-gray-400 lg:border-2" -->
+
   <div>
-    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
+    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-2 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
       {#each round1 as game}
         <li>
-        <div class="bg-white mx-4 mt-4 w-48 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
+        <div class="w-40 whitespace-nowrap bg-white mx-4 mt-4 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
           <a href={`/${game.id}`}>
             <div class="flex flex-col p-4">
             <div class="flex">
@@ -47,10 +46,10 @@
   </div>
 
   <div>
-    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
+    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-2 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
       {#each round2 as game}
         <li>
-        <div class="bg-white mx-4 mt-4 w-48 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
+        <div class="w-40 whitespace-nowrap bg-white mx-4 mt-4 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
           <a href={`/${game.id}`}>
             <div class="flex flex-col p-4">
             <div class="flex">
@@ -81,26 +80,26 @@
   </div>
 
   <div>
-    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-12 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
+    <ul class="py-6 mx-4 grid grid-cols-2 gap-x-2 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-6xl lg:gap-x-8 lg:gap-y-8 xl:grid-cols-6">
       {#each round3 as game}
         <li>
-        <div class="bg-white mx-4 mt-4 w-48 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
-          <a href={`/${game.id}`}>
-            <div class="flex flex-col p-4">
-            <div class="flex">
-              <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag1}`} alt={game.team1} />
-              <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag2}`} alt={game.team2} />
-            </div>
-            <div class="space-y-2">
-              <div class="text-xs font-medium lg:text-sm text-center">
-                <h3>
-                  <div class="text-black">{game.team1} - {game.team2}</div>
-                  <div class="text-black">{game.result}</div>
-                </h3>
+          <div class="w-40 whitespace-nowrap bg-white mx-4 mt-4 select-none cursor-pointer rounded-lg shadow-lg hover:bg-tertiary-500 hover:border-gray-700 transition duration-500 ease-in-out">
+            <a href={`/${game.id}`}>
+              <div class="flex flex-col p-4">
+              <div class="flex">
+                <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag1}`} alt={game.team1} />
+                <img class="mx-auto h-10 w-10 rounded-full object-cover" src={`/images/countries/${game.flag2}`} alt={game.team2} />
               </div>
-            </div>
-            </div>
-          </a>
+              <div class="space-y-2">
+                <div class="whitespace-nowrap text-xs font-medium lg:text-sm text-center">
+                  <h3>
+                    <div class="text-black">{game.team1} - {game.team2}</div>
+                    <div class="text-black">{game.result}</div>
+                  </h3>
+                </div>
+              </div>
+              </div>
+            </a>
           </div>
         </li>
       {/each}
